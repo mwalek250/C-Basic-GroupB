a11d0406aa72cb1db9070bd9a2d4b840ab22d96f
mwalek250 <mwalek250.gmail.com>
2025-09-30 11:17:34 +0000

    Day2: Variables and Data type

--- area_rectangle.c
+++ area_rectangle.c
@@ -4,9 +4,9 @@ int main()
 float length;
 float width;
 float area;
-printf("Enter Length:\n");
+printf("Enter Length :\n");
 scanf("%f", &length);
-printf("Enter Width:\n");
+printf("Enter Width :\n");
 scanf("%f", &width);
 area=length*width;
 printf("Area of a Rectangle:%.2f\n", area);
